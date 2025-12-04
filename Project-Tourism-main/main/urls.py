f054d7830d6d8a6aea191a6ed01fe2f3881a3336
Salin Bade <salinbade1994@gmail.com>
2025-12-04 10:32:21 +0545

    changes are made in authentication template like auth to sam

--- Project-Tourism-main/main/urls.py
+++ Project-Tourism-main/main/urls.py
@@ -19,10 +19,10 @@ urlpatterns = [
     path("logout/", log_out, name='log_out'),
     path("change_password/", change_password, name='change_password'),
     path('user_profile/',user_profile, name='user_profile'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="auth/password_reset.html"), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="auth/password_reset_done.html"), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="auth/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="auth/password_reset_complete.html"), name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="sam/password_reset.html"), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="sam/password_reset_done.html"), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="sam/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="sam/password_reset_complete.html"), name='password_reset_complete'),
     #authentication work ends here
     
     
